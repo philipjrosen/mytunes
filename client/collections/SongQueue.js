e898288e89ddd19a1987bbd8df0fea31bce7c0a2
philipjrosen <philipjrosen@gmail.com>
2014-12-31 16:33:12 -0800

    implements SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,26 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(song){
+      if (this.length ===1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function(song){
+      this.shift();
+      if(this.length) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
